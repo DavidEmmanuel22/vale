7f60ec16f8ce41626466fabe0784d2009fba75f0
Cecilia Flores Durán <ceci@duran.local>
2021-02-11 18:51:06 -0600

    edit styles inicio

--- src/pages/Inicio/Inicio.js
+++ src/pages/Inicio/Inicio.js
@@ -17,9 +17,11 @@ const Inicio = () => {
         <h4 className={classes.BlackText}>
           Necesitas presupuesto para emprender tu negocio?
         </h4>
-        <h1 className={classes.JoinText}>Únete a</h1>
-        <h1 className={classes.ValedorText}>Vale Valedor</h1>
-        <Button size="small">Conoce más</Button>
+        <h1 className={classes.JoinText}>Únete a Vale</h1>
+        <h1 className={classes.ValedorText}>Valedor</h1>
+        <Button className={classes.KnowMore} size="small">
+          Conoce más
+        </Button>
       </div>
       <div>
         <div className={classes.ContentGreen}>
--- src/pages/Inicio/Styles.js
+++ src/pages/Inicio/Styles.js
@@ -25,10 +25,35 @@ const Styles = makeStyles((theme) => ({
   ImageMap: {
     width: '70%'
   },
-  ContentJointo: {},
-  BlackText: {},
-  JoinText: {},
-  ValedorText: {}
+  ContentJointo: {
+    width: '50%',
+    position: 'absolute',
+    top: '26%',
+    margin: '1px 35px'
+  },
+  BlackText: {
+    fontWeight: 'lighter',
+    fontSize: '20px'
+  },
+  JoinText: {
+    fontSize: '90px',
+    fontWeight: '400',
+    margin: '13px 0px'
+  },
+  ValedorText: {
+    fontWeight: '400',
+    fontSize: '90px',
+    color: '#007772'
+  },
+  KnowMore: {
+    backgroundColor: '#efc64f',
+    color: 'white',
+    textTransform: 'capitalize',
+    fontSize: '16px',
+    padding: '4px 10px',
+    borderRadius: '20px',
+    boxShadow: '0 7px 3px rgb(0 0 0 / 12%), 0 1px 2px rgb(0 0 0 / 24%)'
+  }
 }))
 
 export default Styles
