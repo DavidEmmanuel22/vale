17a434b5200a7329d4f55c735a6a5e754032c50f
Cecilia Flores Durán <ceci@duran.local>
2021-02-16 14:55:47 -0600

    fix url login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -25,7 +25,7 @@ const Login = () => {
     try {
       const loginUser = { email, password }
       const loginRes = await Axios.post(
-        'https://devbackend.valevaledor.com/login',
+        'Access-Control-Allow-Origin: https://devbackend.valevaledor.com/login',
         loginUser
       )
       setUserData({
