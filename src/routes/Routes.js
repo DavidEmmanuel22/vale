c0b4d06a28098cd9e960d4d4f434b71812718fba
Cecilia Flores Durán <ceci@duran.local>
2021-02-16 15:53:53 -0600

    fix useContext

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UserContext from 'hooks/UserContext'
 import { BrowserRouter, Switch, Redirect } from 'react-router-dom'
 import Home from 'pages/Home/Home'
 import Login from 'pages/Login/Login'
 import AppRouter from 'components/AppRoute/AppRouter'
 
 const Routes = () => {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined
+  })
+
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect exact from="/" to="/inicio" />
-        <AppRouter path="/:page?" exact component={Home} />
-        <AppRouter path="/login" exact component={Login} />
-      </Switch>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <Switch>
+          <Redirect exact from="/" to="/inicio" />
+          <AppRouter path="/:page?" exact component={Home} />
+          <AppRouter path="/login" exact component={Login} />
+        </Switch>
+      </UserContext.Provider>
     </BrowserRouter>
   )
 }
