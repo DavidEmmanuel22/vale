c8a2f68adfbc24394625bd7547407ef852c72b38
Cecilia Flores Durán <ceci@duran.local>
2021-02-17 17:36:18 -0600

    edit Routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import UserContext from 'hooks/UserContext'
 import { BrowserRouter, Switch } from 'react-router-dom'
-import Home from 'pages/Home/Home'
+// import Home from 'pages/Home/Home'
 // import Login from 'pages/Login/Login'
 import AppRouter from 'components/AppRoute/AppRouter'
 import DashboardLayout from 'components/Layouts/DashboardLayout/DashboardLayout'
+import Inicio from 'pages/Inicio/Inicio'
 
 const Routes = () => {
   const [userData, setUserData] = useState({
@@ -16,7 +17,7 @@ const Routes = () => {
     <BrowserRouter>
       <UserContext.Provider value={{ userData, setUserData }}>
         <Switch>
-          <AppRouter exact path="*" component={Home} />
+          <AppRouter exact path="/" component={Inicio} />
           <AppRouter exact path="/dashboard" component={DashboardLayout} />
         </Switch>
       </UserContext.Provider>
