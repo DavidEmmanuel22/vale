4022899adba08134920a4dcfa98dc4b066bf6b95
Cecilia Flores Durán <ceci@duran.local>
2021-02-17 11:13:32 -0600

    edit Drawer

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,6 +1,5 @@
 import { Drawer, Hidden } from '@material-ui/core'
 import React, { Fragment } from 'react'
-import clsx from 'clsx'
 import Styles from './Styles'
 
 const DrawerLeft = () => {
@@ -8,17 +7,7 @@ const DrawerLeft = () => {
   return (
     <Fragment>
       <Hidden lgUp>
-        <Drawer
-          variant="permanent"
-          className={clsx(classes.drawer, {
-            [classes.drawerClose]: !open
-          })}
-          classes={{
-            paper: clsx({
-              [classes.drawerClose]: !open
-            })
-          }}
-        ></Drawer>
+        <Drawer anchor="left" className={classes.mobileDrawer}></Drawer>
       </Hidden>
     </Fragment>
   )
--- src/components/Drawer/Styles.js
+++ src/components/Drawer/Styles.js
@@ -1,27 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles'
 
-const drawerWidth = 240
 const Styles = makeStyles((theme) => ({
   mobileDrawer: {
-    width: 256
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap'
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
+    width: 240
   }
 }))
 
