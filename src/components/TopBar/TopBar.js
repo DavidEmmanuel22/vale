d4c76edad52729bedf50669c9195ae49a463a5e6
Cecilia Flores Durán <ceci@duran.local>
2021-02-17 18:03:39 -0600

    edit pages,routes

---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import {
+  Tabs,
+  Tab,
+  AppBar,
+  Button,
+  Hidden,
+  SvgIcon,
+  IconButton
+} from '@material-ui/core'
+import { Menu } from '@material-ui/icons'
+import Login from 'pages/Login/Login'
+import PopUp from 'components/Dialog/PopUp'
+import ContactUs from 'pages/ContactUs/Contact'
+import AboutUs from 'pages/AboutUs/AboutUs'
+import Business from 'pages/Business/Business'
+import Valedor from 'pages/Valedor/Valedor'
+import StylesTheme from './StylesTheme'
+import Logo from 'images/logo-appbar.png'
+import Styles from './Styles'
+
+const Home = (props) => {
+  const classes = Styles()
+  const { match, history } = props
+  const { params } = match
+  const { page } = params
+
+  const tabNameToIndex = {
+    0: 'about',
+    1: 'valedor',
+    2: 'negocio',
+    3: 'contáctanos'
+  }
+
+  const indexToTabName = {
+    about: 0,
+    valedor: 1,
+    negocio: 2,
+    contáctanos: 3
+  }
+
+  const [selectedTab, setSelectedTab] = React.useState(indexToTabName[page])
+  const [openDialog, setOpenDialog] = useState(false)
+
+  const handleChange = (event, newValue) => {
+    history.push(`/${tabNameToIndex[newValue]}`)
+    setSelectedTab(newValue)
+  }
+
+  return (
+    <div>
+      <ThemeProvider theme={StylesTheme}>
+        <div className={classes.ContentLogo}>
+          <img className={classes.ImgLogo} src={Logo} alt="logo"></img>
+        </div>
+        <AppBar position="static" color="transparent">
+          <Hidden lgUp>
+            <IconButton color="inherit">
+              <SvgIcon fontSize="large" className={classes.SvgIconHam}>
+                <Menu />
+              </SvgIcon>
+            </IconButton>
+          </Hidden>
+          <Hidden smDown>
+            <Tabs
+              value={selectedTab}
+              onChange={handleChange}
+              indicatorColor="primary"
+            >
+              <Tab value={0} label="Inicio" />
+              <Tab value={1} label="Acerca de" />
+              <Tab value={2} label="Valedor" />
+              <Tab value={3} label="Negocios" />
+              <Tab value={4} label="Contáctanos" />
+            </Tabs>
+          </Hidden>
+        </AppBar>
+        <Hidden smDown>
+          <Button
+            className={classes.ButtonAppBar}
+            size="small"
+            onClick={() => setOpenDialog(true)}
+          >
+            Inicia Sesión
+          </Button>
+        </Hidden>
+      </ThemeProvider>
+      {selectedTab === 0 && <AboutUs />}
+      {selectedTab === 1 && <Valedor />}
+      {selectedTab === 2 && <Business />}
+      {selectedTab === 3 && <ContactUs />}
+      <div>
+        <PopUp openDialog={openDialog} setOpenDialog={setOpenDialog}>
+          <Login />
+        </PopUp>
+      </div>
+    </div>
+  )
+}
+
+export default Home
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,108 +1,32 @@
-import React, { useState } from 'react'
-import { ThemeProvider } from '@material-ui/core/styles'
-import {
-  Tabs,
-  Tab,
-  AppBar,
-  Button,
-  Hidden,
-  SvgIcon,
-  IconButton
-} from '@material-ui/core'
-import { Menu } from '@material-ui/icons'
-import Login from 'pages/Login/Login'
-import PopUp from 'components/Dialog/PopUp'
-import Drawer from 'components/Drawer/Drawer'
-import Inicio from 'pages/Inicio/Inicio'
-import ContactUs from 'pages/ContactUs/Contact'
-import AboutUs from 'pages/AboutUs/AboutUs'
-import Business from 'pages/Business/Business'
-import Valedor from 'pages/Valedor/Valedor'
-import StylesTheme from './StylesTheme'
-import Logo from 'images/logo-appbar.png'
+import React from 'react'
+import Green from 'images/valedor-green.png'
+import Map from 'images/location-map.png'
+import Hero from 'images/valedor-hero.jpg'
+// import { Button, Typography } from '@material-ui/core'
 import Styles from './Styles'
 
-const Home = (props) => {
+const Home = () => {
   const classes = Styles()
-  const { match, history } = props
-  const { params } = match
-  const { page } = params
-
-  const tabNameToIndex = {
-    0: 'inicio',
-    1: 'about',
-    2: 'valedor',
-    3: 'negocio',
-    4: 'contáctanos'
-  }
-
-  const indexToTabName = {
-    inicio: 0,
-    about: 1,
-    valedor: 2,
-    negocio: 3,
-    contáctanos: 4
-  }
-
-  const [selectedTab, setSelectedTab] = React.useState(indexToTabName[page])
-  const [openDialog, setOpenDialog] = useState(false)
-
-  const handleChange = (event, newValue) => {
-    history.push(`/${tabNameToIndex[newValue]}`)
-    setSelectedTab(newValue)
-  }
 
   return (
     <div>
-      <ThemeProvider theme={StylesTheme}>
-        <div className={classes.ContentLogo}>
-          <img className={classes.ImgLogo} src={Logo} alt="logo"></img>
-        </div>
-        <AppBar position="static" color="transparent">
-          <Hidden lgUp>
-            <IconButton color="inherit">
-              <SvgIcon fontSize="large" className={classes.SvgIconHam}>
-                <Menu />
-              </SvgIcon>
-            </IconButton>
-          </Hidden>
-          <Hidden smDown>
-            <Tabs
-              value={selectedTab}
-              onChange={handleChange}
-              indicatorColor="primary"
-            >
-              <Tab value={0} label="Inicio" />
-              <Tab value={1} label="Acerca de" />
-              <Tab value={2} label="Valedor" />
-              <Tab value={3} label="Negocios" />
-              <Tab value={4} label="Contáctanos" />
-            </Tabs>
-          </Hidden>
-        </AppBar>
-        <Drawer />
-        <Hidden smDown>
-          <Button
-            className={classes.ButtonAppBar}
-            size="small"
-            onClick={() => setOpenDialog(true)}
-          >
-            Inicia Sesión
-          </Button>
-        </Hidden>
-      </ThemeProvider>
-      {selectedTab === 0 && <Inicio />}
-      {selectedTab === 1 && <AboutUs />}
-      {selectedTab === 2 && <Valedor />}
-      {selectedTab === 3 && <Business />}
-      {selectedTab === 4 && <ContactUs />}
+      <div className={classes.ContentHero}>
+        <img className={classes.ImageHero} src={Hero} alt="Hero"></img>
+      </div>
       <div>
-        <PopUp openDialog={openDialog} setOpenDialog={setOpenDialog}>
-          <Login />
-        </PopUp>
+        <div className={classes.ContentGreen}>
+          <img className={classes.ImageGreen} src={Green} alt="logo"></img>
+        </div>
+      </div>
+      <div className={classes.ContentMap}>
+        <div className={classes.LocationMap}>
+          <img className={classes.ImageMap} src={Map} alt="Map"></img>
+        </div>
+      </div>
+      <div>
+        <div></div>
       </div>
     </div>
   )
 }
-
 export default Home
