1e8bca25bcec30d09188caf0f50604c8fbed10e8
Cecilia Flores Durán <ceci@duran.local>
2021-02-17 18:46:57 -0600

    edit pages,routes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,31 +1,13 @@
 import React from 'react'
-import Green from 'images/valedor-green.png'
-import Map from 'images/location-map.png'
-import Hero from 'images/valedor-hero.jpg'
+import TopBar from 'components/TopBar/TopBar'
+import Inicio from 'pages/Inicio/Inicio'
 // import { Button, Typography } from '@material-ui/core'
-import Styles from './Styles'
 
 const Home = () => {
-  const classes = Styles()
-
   return (
     <div>
-      <div className={classes.ContentHero}>
-        <img className={classes.ImageHero} src={Hero} alt="Hero"></img>
-      </div>
-      <div>
-        <div className={classes.ContentGreen}>
-          <img className={classes.ImageGreen} src={Green} alt="logo"></img>
-        </div>
-      </div>
-      <div className={classes.ContentMap}>
-        <div className={classes.LocationMap}>
-          <img className={classes.ImageMap} src={Map} alt="Map"></img>
-        </div>
-      </div>
-      <div>
-        <div></div>
-      </div>
+      <TopBar />
+      <Inicio />
     </div>
   )
 }
