c71f538e44767f7f509ab0a385e11e95851d6399
Angel Acevedo <angelacevedo975@gmail.com>
2021-03-16 10:33:00 -0600

    add credit functionallity

--- src/components/credito/register.js
+++ src/components/credito/register.js
@@ -8,9 +8,10 @@ import AccountCircle from '@material-ui/icons/AccountCircle'
 import EmailIcon from '@material-ui/icons/Email'
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn'
 import { AlertContext } from '../popUp/responsivePopUp'
-import { createValedor } from 'requests/allValedores'
+import { addCredit, createValedor } from 'requests/allValedores'
 import { makeStyles } from '@material-ui/core/styles'
 import { createVale } from 'requests/allVales'
+import { addCreditSchema } from 'yupSchemas'
 
 const Styles = makeStyles(() => ({
   widthnew: {
@@ -36,25 +37,6 @@ const Styles = makeStyles(() => ({
   }
 }))
 
-const NameExpression = /^[a-zA-ZÀ-ÿñÑ\s]*$/
-
-const validationSchema = yup.object({
-  firstName: yup
-    .string()
-    .min(5, 'Mínimo 5 caracteres')
-    .max(25, 'Maxímo 25 caracteres')
-    .matches(NameExpression, 'Solo se permiten letras para este campo')
-    .required('Nombre es requerido'),
-  lastName: yup
-    .string()
-    .matches(NameExpression, 'Solo se permiten letras para este campo')
-    .required('Apellido es requerido'),
-  credits: yup
-    .number()
-    // .matches( creditsExpression,"Solo se permiten letras para este campo")
-    .required('Credito es requerido')
-})
-
 const RegisterCredit = (props) => {
   const classes = Styles()
 
@@ -64,46 +46,40 @@ const RegisterCredit = (props) => {
 
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
+      email: '',
       credits: ''
     },
-    onSubmit: async (vale, { resetForm }) => {
-      const some = { ...vale, emailUser: 'angelacevedo975@gmail.com' }
-      console.log(some)
-      const { success, response, error } = await createVale(some)
+    onSubmit: async (values, { resetForm }) => {
+      const { success, response, error } = await addCredit(
+        values.email,
+        values.credits
+      )
       if (success && response) {
-        if (response.msg) {
+        if (response.error) {
           setAlertColor('error')
-          setAlertText(response.msg)
+          setAlertText(response.error)
         } else {
           setAlertColor('success')
-          setAlertText('El vale fue creado correctamente')
-          //resetForm({ vale: '' })
+          setAlertText('Se ha añadido el credito correctamente')
         }
       }
-      console.log(vale)
-      console.log('jejeje')
-      console.log(response)
     },
-    validationSchema: validationSchema
+    validationSchema: addCreditSchema
   })
 
   return (
     <div className="register-vale">
       <form className={classes.root} onSubmit={formik.handleSubmit}>
         <Grid container spacing={3}>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12}>
             <TextField
               className={classes.widthnew}
-              id="firstName"
-              placeholder="Nombre"
-              value={formik.values.firstName}
+              id="email"
+              placeholder="Correo electronico"
+              value={formik.values.email}
               onChange={formik.handleChange}
-              error={
-                formik.touched.firstName && Boolean(formik.errors.firstName)
-              }
-              helperText={formik.touched.firstName && formik.errors.firstName}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
               type="text"
               InputProps={{
                 startAdornment: (
@@ -114,27 +90,7 @@ const RegisterCredit = (props) => {
               }}
             />
           </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              className={classes.widthnew}
-              id="lastName"
-              placeholder="Apellido"
-              value={formik.values.lastName}
-              onChange={formik.handleChange}
-              error={formik.touched.lastName && Boolean(formik.errors.lastName)}
-              helperText={formik.touched.lastName && formik.errors.lastName}
-              type="text"
-              // onChange={(event) => setLastName(event.target.value)}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <AccountCircle />
-                  </InputAdornment>
-                )
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12}>
             <TextField
               className={classes.widthnew}
               id="credits"
@@ -163,7 +119,7 @@ const RegisterCredit = (props) => {
                 type="submit"
                 color="primary"
               >
-                Registrar Valedor{' '}
+                Agregar Credito{' '}
               </Button>
             </div>
           </Grid>
--- src/yupSchemas/index.js
+++ src/yupSchemas/index.js
@@ -9,6 +9,7 @@ const MAX_LAST_NAME = 35
 export const updateUserSelfSchema = yup.object({
   firstName: yup
     .string()
+    .trim('Sin espacios al principio')
     .required('El nombre es requerido')
     .min(
       MIN_FIRST_NAME,
@@ -17,9 +18,11 @@ export const updateUserSelfSchema = yup.object({
     .max(
       MAX_FIRST_NAME,
       `El nombre debe tener maximo ${MAX_FIRST_NAME} caracteres`
-    ),
+    )
+    .matches(/^[a-zA-ZÀ-ÿñÑ\s]*$/, 'Nombre invalido'),
   lastName: yup
     .string()
+    .trim()
     .required('El apellido es requerido')
     .min(
       MIN_LAST_NAME,
@@ -29,4 +32,13 @@ export const updateUserSelfSchema = yup.object({
       MAX_LAST_NAME,
       `El apellido debe tener maximo ${MAX_LAST_NAME} caracteres`
     )
+    .matches(/^[a-zA-ZÀ-ÿñÑ\s]*$/, 'Apellido invalido')
+})
+
+export const addCreditSchema = yup.object({
+  email: yup
+    .string()
+    .required('El correo electronico es requerido')
+    .email('El correo electronico es invalido'),
+  credits: yup.number().positive('El credito debe ser positivo')
 })
