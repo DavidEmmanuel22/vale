4a02e2b9734c669396ed6e9c383c8a1a9d862cc3
Angel Acevedo <angelacevedo975@gmail.com>
2021-06-03 10:11:59 -0500

    fix regex expression for names in contact messages

--- src/pages/Contact/ContactValidation.js
+++ src/pages/Contact/ContactValidation.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-useless-escape */
 import * as yup from 'yup'
 
 export const contactValidation = yup.object({
-  name: yup.string().required('Nombre requerido'),
+  name: yup
+    .string()
+    .required('Nombre requerido')
+    .matches(/^[a-zA-ZÀ-ÿñÑ\s]*$/, 'El nombre contiene caractéres invalidos'),
   email: yup
     .string()
     .email('Correo electrónico inválido')
@@ -14,7 +18,10 @@ export const contactValidation = yup.object({
 })
 
 export const loginValidation = yup.object({
-  name: yup.string().required('Nombre requerido'),
+  name: yup
+    .string()
+    .required('Nombre requerido')
+    .matches(/^[a-zA-ZÀ-ÿñÑ\s]*$/, 'El nombre contiene caractéres invalidos'),
   email: yup
     .string()
     .email('Correo electrónico inválido')
