275dbb3349a5acbd29d94946bf3c3d802b2fb2a4
Angel Acevedo <angelacevedo975@gmail.com>
2021-06-29 12:03:45 -0500

    fix delete image button

--- src/pages/DashboardPerfil/Avatar.jsx
+++ src/pages/DashboardPerfil/Avatar.jsx
@@ -4,6 +4,7 @@ import Avatar from '@material-ui/core/Avatar'
 import { makeStyles } from '@material-ui/core/styles'
 import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate'
 import DeleteIcon from '@material-ui/icons/Delete';
+import { Button } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -28,7 +29,7 @@ export default function BadgeAvatars({ image, onEdit, setImageUrl, setImgData })
 	}
 
 	return (
-		<div style={{ justifyContent: 'center' }} className={classes.root}>
+		<div style={{ display:"flex", flexDirection:"column", }} className={classes.root}>
 			<label
 				style={{ cursor: "pointer" }}
 				htmlFor="contained-button-file">
@@ -43,15 +44,18 @@ export default function BadgeAvatars({ image, onEdit, setImageUrl, setImgData })
 							<label htmlFor="contained-button-file" style={{ cursor: "pointer", display: onEdit ? "block" : "none" }}>
 								<AddPhotoAlternateIcon color="secondary" />
 							</label>
+							{/*
 							<label style={{ cursor: "pointer", display: !onEdit ? "none" : image === "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" ? "none" : "block" }} onClick={handleDeleteImage}>
 								<DeleteIcon color="primary" />
 							</label>
+							*/}
 						</>
 					}
 				>
 					<Avatar alt="Profile Image" className={classes.large} src={image} />
 				</Badge>
 			</label>
+			<a color="primary" variant="contained" style={{ cursor: "pointer", display: !onEdit ? "none" : image === "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" ? "none" : "block" }} onClick={handleDeleteImage}>Eliminar Imagen</a>
 		</div>
 	)
 }
