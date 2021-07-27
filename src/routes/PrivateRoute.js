3548e9b3699ef1b2fabaf7984611eef890ec84c0
Angel Acevedo <angelacevedo975@gmail.com>
2021-07-27 12:32:03 -0500

    fix routing bugs

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,33 +1,23 @@
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
 const PrivateRoute = ({ children, isAuthenticated, ...rest }) => {
-  const refToScroll = useRef(null)
-
-  const scrollToTop = () => {
-    refToScroll.current.scrollIntoView({ behavior: 'smooth' })
-  }
-  useEffect(() => {
-    scrollToTop()
-  }, [])
   return (
-    <div ref={refToScroll}>
-      <Route
-        {...rest}
-        render={({ location }) =>
-          isAuthenticated ? (
-            children
-          ) : (
-            <Redirect
-              to={{
-                pathname: '/',
-                state: { from: location }
-              }}
-            />
-          )
-        }
-      />
-    </div>
+    <Route
+      {...rest}
+      render={({ location }) =>
+        isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/',
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
   )
 }
 
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,111 +1,101 @@
-import React, { useContext } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { useContext, useRef, useEffect } from 'react'
+import { Switch, Route, useLocation, Redirect } from 'react-router-dom'
 import { ClientNavBar, Home } from '../pages/Home/Home'
-import { Dashboard } from 'pages/Dashboard/Dashboard'
 import ForgotPassword from 'components/ForgotPasword/ForgotPassword'
 import UpdatePassword from 'components/ResetPassword/UpdatePassword'
-import { DashboardPerfil } from 'pages/DashboardPerfil/DashboardPerfil'
 import GeneralLayout from 'components/Layouts/DashboardLayout/GeneralLayout'
 import Historial from 'pages/valedores/Historial'
 import { UserContext } from '../context/userContext'
 import PrivateRoute from './PrivateRoute'
-import Valedores from 'pages/valedores/Valedores'
-import Negocios from 'pages/negocios/Negocios'
 import Contact from 'pages/Contact/Contact'
 import dashboardRoutes from './dashboardRoutes'
 import valedorRoutes from './valedorRoutes'
 import BussinesRoutes from './bussinesRoutes'
-import historyRoute from './history'
 import { Mail } from 'pages/Mail/Mail'
 import { Vale } from 'pages/valedores/Vale'
 import { Business } from 'components/Business/Business'
 
 const Routes = () => {
   const { isAuthenticated, user } = useContext(UserContext)
+  const refToScroll = useRef(null)
+  const path = useLocation()
 
+  const scrollToTop = () => {
+    refToScroll.current.scrollIntoView({ behavior: 'smooth' })
+  }
+  useEffect(() => {
+    scrollToTop()
+  }, [path])
   return (
-    <Switch>
-      {user.role === 'Admin' &&
-        dashboardRoutes.map((route, index) => (
-          <PrivateRoute
-            key={index}
-            isAuthenticated={isAuthenticated}
-            exact
-            path={route.path}
-          >
-            <GeneralLayout routes={dashboardRoutes}>
-              <route.component></route.component>
-            </GeneralLayout>
-          </PrivateRoute>
-        ))}
-      {user.role === 'Valedor' &&
-        valedorRoutes.map((route, index) => (
-          <PrivateRoute
-            key={index}
-            isAuthenticated={isAuthenticated}
-            exact
-            path={route.path}
-          >
-            <GeneralLayout routes={valedorRoutes}>
-              <route.component></route.component>
-            </GeneralLayout>
-          </PrivateRoute>
-        ))}
-      {user.role === 'Bussines' &&
-        BussinesRoutes.map((route, index) => (
-          <PrivateRoute
-            key={index}
-            isAuthenticated={isAuthenticated}
-            exact
-            path={route.path}
-          >
-            <GeneralLayout routes={valedorRoutes}>
-              <route.component></route.component>
-            </GeneralLayout>
-          </PrivateRoute>
-        ))}
+    <div ref={refToScroll}>
+      <Switch>
+        {user.role === 'Admin' &&
+          dashboardRoutes.map((route, index) => (
+            <PrivateRoute key={index} isAuthenticated={isAuthenticated} exact path={route.path}>
+              <GeneralLayout routes={dashboardRoutes}>
+                <route.component></route.component>
+              </GeneralLayout>
+            </PrivateRoute>
+          ))}
+        {user.role === 'Valedor' &&
+          valedorRoutes.map((route, index) => (
+            <PrivateRoute key={index} isAuthenticated={isAuthenticated} exact path={route.path}>
+              <GeneralLayout routes={valedorRoutes}>
+                <route.component></route.component>
+              </GeneralLayout>
+            </PrivateRoute>
+          ))}
+        {user.role === 'Bussines' &&
+          BussinesRoutes.map((route, index) => (
+            <PrivateRoute key={index} isAuthenticated={isAuthenticated} exact path={route.path}>
+              <GeneralLayout routes={valedorRoutes}>
+                <route.component></route.component>
+              </GeneralLayout>
+            </PrivateRoute>
+          ))}
 
-      <PrivateRoute
-        exact
-        isAuthenticated={isAuthenticated}
-        path="/valedores/history"
-      >
-        <GeneralLayout routes={dashboardRoutes}>
-          <Historial />
-        </GeneralLayout>
-      </PrivateRoute>
+        <PrivateRoute exact isAuthenticated={isAuthenticated} path='/valedores/history'>
+          <GeneralLayout routes={dashboardRoutes}>
+            <Historial />
+          </GeneralLayout>
+        </PrivateRoute>
 
-      <Route exact path="/forgot-password">
-        <ForgotPassword />
-      </Route>
+        <Route exact path='/forgot-password'>
+          <ForgotPassword />
+        </Route>
 
-      <Route exact path="/update-password/:token">
-        <UpdatePassword />
-      </Route>
+        <Route exact path='/update-password/:token'>
+          <UpdatePassword />
+        </Route>
 
-      <Route exact path="/mail">
-        <ClientNavBar />
-        <Mail />
-      </Route>
+        <Route exact path='/mail'>
+          <ClientNavBar />
+          <Mail />
+        </Route>
 
-      <Route exact path="/business">
-        <Business />
-      </Route>
+        <Route exact path='/business'>
+          <Business />
+        </Route>
 
-      <Route exact path="/contact">
-        <Contact />
-      </Route>
+        <Route exact path='/contact'>
+          <Contact />
+        </Route>
 
-      <Route exact path="/vale">
-        <GeneralLayout routes={valedorRoutes}>
-          <Vale />
-        </GeneralLayout>
-      </Route>
+        <Route exact path='/vale'>
+          <GeneralLayout routes={valedorRoutes}>
+            <Vale />
+          </GeneralLayout>
+        </Route>
 
-      <Route exact path="/">
-        <Home />
-      </Route>
-    </Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+        <Route path='/'>
+          <Redirect to='/'></Redirect>
+        </Route>
+      </Switch>
+    </div>
   )
 }
 
