4dddab7c8cd4f0b47a3997272c53eaa210708eb2
David Valdez <davidvaldez@MacBook-Air-de-David.local>
2021-08-19 21:18:16 -0500

    add validation in case of lastMessage is undefined

--- src/components/DashboardMessages/NewMessage.js
+++ src/components/DashboardMessages/NewMessage.js
@@ -7,30 +7,35 @@ import 'moment/min/locales'
 import CloseIcon from '@material-ui/icons/Close'
 import { Mail } from 'pages/Mail/Mail'
 moment.locale('es')
-
 const NewMessage = ({ chat, clicked, readed, id }) => {
-    return (
-        <div
-            key={id}
-            onClick={() => clicked()}
-            className='message-container'
-            style={chat.readAdmin ? { backgroundColor: 'gainsboro' } : { backgroundColor: '#f9a02b94' }}
-        >
-            <span>{chat.roleChat === 'Invited' ? `${'Invitado'}` : chat.roleChat}</span>
-            <div key={id} className='data-container'>
-                <img width='50' src='/images/no-avatar.png'></img>
+    if (!chat.lastMessage) {
+        return null
+    } else {
+        return (
+            <div
+                key={id}
+                onClick={() => clicked()}
+                className='message-container'
+                style={chat.readAdmin ? { backgroundColor: 'gainsboro' } : { backgroundColor: '#f9a02b94' }}
+            >
+                <span>{chat.roleChat === 'Invited' ? `${'Invitado'}` : chat.roleChat}</span>
+                <div key={id} className='data-container'>
+                    <img width='50' src='/images/no-avatar.png'></img>
 
-                <div key={id}>
-                    <span>{chat.name}</span>
-                    <p style={!chat.readAdmin ? { fontWeight: 'bold' } : null}>{chat.lastMessage.substring(0, 50)}</p>
+                    <div key={id}>
+                        <span>{chat.name}</span>
+                        <p style={!chat.readAdmin ? { fontWeight: 'bold' } : null}>
+                            {chat.lastMessage.substring(0, 50)}
+                        </p>
+                    </div>
+
+                    <span style={chat.readAdmin ? { fontWeight: 'bold', backgroundColor: 'gainsboro' } : null}>
+                        {moment(chat.lastDateMessage).format('MM/DD/YYYY  hh:mm A')}
+                    </span>
                 </div>
-
-                <span style={chat.readAdmin ? { fontWeight: 'bold', backgroundColor: 'gainsboro' } : null}>
-                    {moment(chat.lastDateMessage).format('MM/DD/YYYY  hh:mm A')}
-                </span>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default NewMessage
