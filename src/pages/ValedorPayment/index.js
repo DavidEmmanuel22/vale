04a9e6285c2db5003afde9ffc3116202ee8a13d3
Angel Acevedo <angelacevedo@Angels-MacBook-Air.local>
2021-10-13 10:11:37 -0500

    integrate valedor vayment history page

--- src/pages/ValedorPayment/index.js
+++ src/pages/ValedorPayment/index.js
@@ -1,8 +1,12 @@
-import { Grid, makeStyles, Paper, Typography } from '@material-ui/core'
+import { Grid, IconButton, makeStyles, Paper, Popover, Tooltip, Typography } from '@material-ui/core'
 import React from 'react'
 import { useParams, useLocation } from 'react-router-dom'
 import _ from 'lodash'
 import ValedorPaymentTable from 'components/Tables/ValedorPaymentTable'
+import { Alert } from '@material-ui/lab'
+import { FilterList } from '@material-ui/icons'
+import DateRange from 'components/filters/DateRangePicker'
+import { RowProvider } from 'assets/Helpers/RowContext'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -12,13 +16,13 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
-        height: `calc(100vh - 172px)`,
+        height: `calc(100vh - 185px)`,
         overflowY: 'scroll'
     },
     titlePaper: {
         padding: theme.spacing(2),
         display: 'flex',
-        justifyContent: 'flex-start',
+        justifyContent: 'space-between',
         alignItems: 'center',
         minHeight: '65px'
     },
@@ -31,7 +35,47 @@ const ValedorPayment = () => {
     const { idValedor } = useParams()
     const location = useLocation()
 
+    const [anchorEl, setAnchorEl] = React.useState(null)
+
+    const [state, setState] = React.useState([
+        {
+            startDate: null,
+            endDate: null,
+            key: 'selection'
+        }
+    ])
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
+    const open = Boolean(anchorEl)
+    const id = open ? 'simple-popover' : undefined
+
+    function handleSelectRange(item) {
+        setState([item.selection])
+    }
+
+    function handleSelectAll() {
+        console.log('select all')
+        setState([
+            {
+                startDate: null,
+                endDate: null,
+                key: 'selection'
+            }
+        ])
+    }
+
     const classes = useStyles()
+
+    const noRowsComponent = (
+        <Alert severity='info'>¡Ups! Parece que no existen registros de pagos en las fechas seleccionadas</Alert>
+    )
 
     React.useEffect(() => {
         console.log(location)
@@ -45,11 +89,38 @@ const ValedorPayment = () => {
                         Historial de Pagos - {_.get(location, 'state.valedor.firstName')}{' '}
                         {_.get(location, 'state.valedor.lastName')}
                     </Typography>
+                    <Tooltip title='Mostrar filtros' onClick={handleClick} style={{ marginRight: '30px' }}>
+                        <IconButton aria-describedby={id}>
+                            <FilterList color='primary'></FilterList>
+                        </IconButton>
+                    </Tooltip>
+                    <Popover
+                        id={id}
+                        open={open}
+                        anchorEl={anchorEl}
+                        onClose={handleClose}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left'
+                        }}
+                        style={{ overflow: 'scroll' }}
+                    >
+                        <DateRange
+                            date={state}
+                            onRangeChange={handleSelectRange}
+                            onSelectAll={handleSelectAll}
+                        ></DateRange>
+                    </Popover>
                 </Paper>
             </Grid>
             <Grid item xs={12}>
                 <Paper className={classes.mainPaper}>
-                    <ValedorPaymentTable></ValedorPaymentTable>
+                    <RowProvider component={noRowsComponent}>
+                        <ValedorPaymentTable
+                            dateRange={state}
+                            valedor={_.get(location, 'state.valedor', {})}
+                        ></ValedorPaymentTable>
+                    </RowProvider>
                 </Paper>
             </Grid>
         </Grid>
